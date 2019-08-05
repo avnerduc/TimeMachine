fb5a7c16eafcaa5ab3aef398f96bd5c45bf51ae3
Michael-Steshenko <19752384+Michael-Steshenko@users.noreply.github.com>
2019-08-05 15:39:09 +0300

    Update time_machine.bat

--- time_machine.bat
+++ time_machine.bat
@@ -1,1 +1,4 @@
+#need to make the script ask the user for the location of the main python file
+#for now it is hardcoded 
+#may also get location from an installation process when available
 START /MIN python C:\Users\suppman\Desktop\time_machine\0.1.4.py
